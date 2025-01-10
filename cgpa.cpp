888675d0628ecff4350cf736fd7e31ada6ab79fe
Aithani24 <gauravaithani7@gmail.com>
2025-01-10 16:50:32 +0530

    Add files via upload

---
+++ cgpa.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    int sub;
+    float percentage,cg,marks,total=0,maxmarks;    
+    cout<<"Number of subjects you want to Calculate the CGPA of \n";
+    cin>> sub;
+    cout<< "Enter the maximum marks that a student can take \n";
+    cin>> maxmarks;
+    for (int i = 1; i <= sub; i++)
+    {
+        cout<< "Enter the marks of subject "<< i <<"\n";
+        cin>> marks;
+        total=total+marks;
+    }
+    cout<< "Total marks obtained " <<total <<"\n";
+    if (maxmarks<total)
+    {
+        cout<<"Invalid Data";
+    }
+    else
+    {
+        percentage= (total/maxmarks)*100;
+        cg= (percentage/9.5);
+        cout<<fixed<<setprecision(2)<<"Your Percentage "<<percentage<<"% \n";
+        cout<<fixed<<setprecision(2)<<"Your CGPA "<< cg <<"\n";
+    }
+
+    
+ 
+
+}
